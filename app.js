99ead269aa3ece33624ed7b099f17c596e88eb7a
Fiery <85811433+Fiery-1@users.noreply.github.com>
2023-12-09 17:03:23 +0000

    Added POST to the form on the goals page

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require("express")
 const app = express()
 const port = 3000;
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 app.use(express.static("public"));
 
-app.listen(port, () => {console.log(`App listening port: ${port}`)})
+app.listen(port, () => {console.log(`App listening on port: ${port}`)})
+
+app.post('/form', jsonParser, (req, res) => {
+    const body = req.body;
+    const firstName = body.firstName;
+    const surname = body.surname;
+    const email = body.email;
+    const comments = body.comments;
+    res.send(`POST by form.js - first name =${firstName}  , surname = ${surname},  email = ${email},  comments = ${comments}`)
+});
