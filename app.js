cb536733d74d9ed1e978ea67654f97faf1d9b8e1
Fiery <85811433+Fiery-1@users.noreply.github.com>
2023-12-11 23:54:53 +0000

    Working fetch

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
 const port = 3000;
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+app.use(bodyParser.json());
+app.use(express.static('public')); // files are in public folder
 
-const fs = require("fs");
-
-app.use(express.static("public"));
-
-app.post('/newsletter', jsonParser, (req, res) => {
-        const body = req.body;
-        const firstName = body.firstName;
-        const lastName = body.lastName;
-        const email = body.email;
-        const comments = body.comments;
-        const formSubmission = {
-            message: "POST by form.js",
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            comments: comments
-        };
-
-        const path = "formSubmission.json";
-        console.log("CHECK")
-        console.log("form data: ",formSubmission);
-        fs.writeFile(path, JSON.stringify(formSubmission), (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ error: 'Internal Server Error' });
-            } else {
-                console.log('Form data saved successfully');
-                res.json({ message: 'Form data saved successfully' });
-            }
-        });
-        // res.sendFile("/Users/archa/Desktop/GitHub/Sustainability-Advisory-Comittee/public/newsletter.html")
+app.get('/goals.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'goals.html'));
 });
 
-app.listen(port, () => {console.log(`App listening on port: ${port}`)});
+app.post('/form', (req, res) => {
+  const body = req.body;
+  
+  const firstName = body.firstName;
+  const surname = body.surname;
+  const email = body.email;
+  const message = body.message;
+
+  // Read existing data from the file
+  fs.readFile('body.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading body.json:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    let existingData;
+    try {
+      existingData = data ? JSON.parse(data) : [];
+      if (!Array.isArray(existingData)) {
+        throw new Error('Existing data is not an array');
+      }
+    } catch (parseError) {
+      console.error('Error parsing existing data:', parseError);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    // Append the new form data
+    existingData.push(body);
+
+    // Write the updated data back to the file
+    fs.writeFile('body.json', JSON.stringify(existingData), (writeErr) => {
+      if (writeErr) {
+        console.error('Error writing body.json:', writeErr);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      console.log('Form data appended to body.json');
+      res.send(`Hi ${firstName} ${surname}, thanks for signing up. \n A confirmation email has been sent to: ${email}`);
+
+    });
+  });
+});
+
+// For all other routes, serve an 'index.html' or any other default page
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
--- public/form.js
+++ public/form.js
@@ -1,4 +1,5 @@
 const submit = document.getElementById('submit-footer');
+var signedUp = false;
 
 function processSubmit(e){
     e.preventDefault();
@@ -6,7 +7,6 @@ function processSubmit(e){
         let newLine = document.createElement('p');
         let currentLine = document.getElementById('test-response');
         newLine.textContent = text;
-        newLine.style.cssText = "color: red; position: absolute;";
         currentLine.append(newLine);
         console.log(text);
     }
@@ -19,34 +19,36 @@ function processSubmit(e){
     const formSurname = document.getElementById('surname').value;
     const formEmail = document.getElementById('email').value;
     const formComments = document.getElementById('comments').value;
+    if (signedUp == false){
+        if (formFirstName && formSurname && formEmail && formComments != null) {
+            console.log('formFirstName:', formFirstName);
+            console.log('formSurname:', formSurname);
+            console.log('formEmail:', formEmail);
+            console.log('formComments:', formComments);
 
-    if (formFirstName && formSurname && formEmail && formComments != null) {
-        console.log('formFirstName:', formFirstName);
-        console.log('formSurname:', formSurname);
-        console.log('formEmail:', formEmail);
-        console.log('formComments:', formComments);
-
-        const fullMessage = {
-            firstName: formFirstName,
-            surname: formSurname,
-            email: formEmail,
-            comments: formComments
-        };
-        
-        const serializedMessage = JSON.stringify(fullMessage);
-        
-        const fetchOptions ={
-            method: 'POST',
-            headers:{
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: serializedMessage
+            const fullMessage = {
+                firstName: formFirstName,
+                surname: formSurname,
+                email: formEmail,
+                comments: formComments
+            };
+            
+            const serializedMessage = JSON.stringify(fullMessage);
+            
+            const fetchOptions ={
+                method: 'POST',
+                headers:{
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: serializedMessage
+            }
+            
+            fetch('http://localhost:3000/form', fetchOptions)
+            .then(onResponse)
+            .then(onTextReady);
+            signedUp = true;
         }
-        
-        fetch('http://localhost:3000/form', fetchOptions)
-        .then(onResponse)
-        .then(onTextReady);
     }
 }
 
