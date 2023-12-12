5864671f409f92b8e283d6507785aa07780f5d06
Fiery <85811433+Fiery-1@users.noreply.github.com>
2023-12-12 00:23:54 +0000

    Refined POST code

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public')); // files are in public folder
+app.use(express.static('public')); // public directory
 
-app.get('/goals.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'goals.html'));
-});
+
+app.get('*', (req, res) => {  // \       * to select all pages
+    res.sendFile(path.join('public', 'index.html'));
+  });
+  
 
 app.post('/form', (req, res) => {
   const body = req.body;
@@ -23,47 +25,24 @@ app.post('/form', (req, res) => {
 
   // Read existing data from the file
   fs.readFile('body.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading body.json:', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-
     let existingData;
-    try {
-      existingData = data ? JSON.parse(data) : [];
-      if (!Array.isArray(existingData)) {
-        throw new Error('Existing data is not an array');
+    if (data) {
+        existingData = JSON.parse(data);
+      } else {
+        existingData = [];
       }
-    } catch (parseError) {
-      console.error('Error parsing existing data:', parseError);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
 
-    // Append the new form data
     existingData.push(body);
 
     // Write the updated data back to the file
     fs.writeFile('body.json', JSON.stringify(existingData), (writeErr) => {
-      if (writeErr) {
-        console.error('Error writing body.json:', writeErr);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-
       console.log('Form data appended to body.json');
-      res.send(`Hi ${firstName} ${surname}, thanks for signing up. \n A confirmation email has been sent to: ${email}`);
+      res.send(`Hi ${firstName} ${surname}, thanks for signing up. A confirmation email has been sent to: ${email}`);
 
     });
   });
 });
 
-// For all other routes, serve an 'index.html' or any other default page
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on: http://localhost:${port}`);
 });
